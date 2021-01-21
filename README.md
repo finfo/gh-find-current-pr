7430c1c0cffab2d3e9f827a096c9245f3fd46e2a
alanyeh20001 <alanyeh20001@hotmail.com>
2021-01-21 16:40:45 +0800

    - Find PR which associated with current commit or merged branch
    - Output PR labels which concatenated by line break
    
    - Update README.md

--- README.md
+++ README.md
@@ -5,25 +5,25 @@ This action tries to figure out the current PR.
 If the event is a `pull_request`, it's very easy to get the current PR number
 from the context via `${{ github.event.number }}`, but unfortunately this
 information does not seem to be readily available for a `push` event.  This
-action sends a request to GitHub to find the PR associated with the current SHA,
+action sends a request to GitHub to find the PR associated with the current SHA or merged branch,
 and returns its number in the `number` output. `number` will be an empty string if there is no
 PR.
 
-Additionally, `title` and `body` outputs are available as well to get the respective title and body of the PR.
+Additionally, `title`, `body` and `labels` outputs are available as well to get the respective title, body and labels of the PR.
 
 ## Usage
 
 ```yaml
     steps:
-      - uses: actions/checkout@v1
-      # Find the PR associated with this push, if there is one.
-      - uses: jwalton/gh-find-current-pr@v1
+      - uses: actions/checkout@v2
+      # Find the PR associated with this push, either with current commit or merged branch.
+      - uses: finfo/gh-find-current-pr@master
         id: findPr
         with:
           github-token: ${{ secrets.GITHUB_TOKEN }}
-      # This will echo "Your PR is 7", or be skipped if there is no current PR.
-      - run: echo "Your PR is ${PR}"
+      # This will echo "Your PR number is 7", or be skipped if there is no current PR.
+      - run: echo "Your PR number is ${PR_Number}"
         if: success() && steps.findPr.outputs.number
         env:
-          PR: ${{ steps.findPr.outputs.pr }}
+          PR_Number: ${{ steps.findPr.outputs.number }}
 ```
--- main.js
+++ main.js
@@ -4,20 +4,35 @@ const { GitHub, context } = require('@actions/github');
 async function main() {
     const token = core.getInput('github-token', { required: true });
     const sha = core.getInput('sha');
-
     const client = new GitHub(token, {});
-    const result = await client.repos.listPullRequestsAssociatedWithCommit({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        commit_sha: sha || context.sha,
-    });
+    let pr;
 
-    const pr = result.data.length > 0 && result.data[0];
-
-    core.setOutput('pr', pr && pr.number || '');
+    pr = await getPullRequestByCommit(client, context, sha);
+    if (pr === undefined) {
+        const response = await client.repos.getCommit({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            ref: sha || context.sha
+        });
+        const currentCommit = response.data;
+        const mergedAncestorCommit = currentCommit.parents[1];
+        if (mergedAncestorCommit) {
+            pr = await getPullRequestByCommit(client, context, mergedAncestorCommit.sha);
+        }
+    }
     core.setOutput('number', pr && pr.number || '');
     core.setOutput('title', pr && pr.title || '');
     core.setOutput('body', pr && pr.body || '');
+    core.setOutput('labels', pr && pr.labels.map(l => l.name).join('\n') || '');
+}
+
+async function getPullRequestByCommit(client, context, sha) {
+    const response = await client.repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: sha || context.sha
+    });
+    if (response.data.length > 0 && response.data[0]) { return response.data[0]; }
 }
 
 main().catch(err => core.setFailed(err.message));
