0a58be75a31543111770d66f8f62df4316076972
alanyeh20001 <alanyeh20001@hotmail.com>
2023-03-30 11:59:46 +0800

    - Upgrade @actions/core to v1.10.0
    - Upgrade @actions/github to v5.1.1
    
    - Update action to run on node16
    
    - Update syntax in main.js
    
    - Add .gitignore and delete existing files under node_modules directory

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
-const { GitHub, context } = require('@actions/github');
+const github = require('@actions/github');
 
 async function main() {
     const token = core.getInput('github-token', { required: true });
-    const sha = core.getInput('sha');
-    const client = new GitHub(token, {});
+    const sha = core.getInput('sha', { required: false });
+    const octokit = github.getOctokit(token);
+    const context = github.context;
     let pr;
 
-    pr = await getPullRequestByCommit(client, context, sha);
+    pr = await getPullRequestByCommit(octokit, context, sha);
     if (pr === undefined) {
-        const response = await client.repos.getCommit({
+        const response = await octokit.rest.repos.getCommit({
             owner: context.repo.owner,
             repo: context.repo.repo,
             ref: sha || context.sha
@@ -17,17 +18,17 @@ async function main() {
         const currentCommit = response.data;
         const mergedAncestorCommit = currentCommit.parents[1];
         if (mergedAncestorCommit) {
-            pr = await getPullRequestByCommit(client, context, mergedAncestorCommit.sha);
+            pr = await getPullRequestByCommit(octokit, context, mergedAncestorCommit.sha);
         }
     }
-    core.setOutput('number', pr && pr.number || '');
-    core.setOutput('title', pr && pr.title || '');
-    core.setOutput('body', pr && pr.body || '');
-    core.setOutput('labels', pr && pr.labels.map(l => l.name).join('\n') || '');
+    core.setOutput('number', (pr && pr.number) || '');
+    core.setOutput('title', (pr && pr.title) || '');
+    core.setOutput('body', (pr && pr.body) || '');
+    core.setOutput('labels', (pr && pr.labels.map(l => l.name).join('\n')) || '');
 }
 
-async function getPullRequestByCommit(client, context, sha) {
-    const response = await client.repos.listPullRequestsAssociatedWithCommit({
+async function getPullRequestByCommit(octokit, context, sha) {
+    const response = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
         owner: context.repo.owner,
         repo: context.repo.repo,
         commit_sha: sha || context.sha
